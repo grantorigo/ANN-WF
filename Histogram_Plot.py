e3da63a536ec5e8773bdbb49373a45ca6a087a12
Jonas <rigojonas@gmail.com>
2018-10-03 15:23:14 +0200

    Finished plot

--- Histogram_Plot.py
+++ Histogram_Plot.py
@@ -22,31 +22,24 @@ with open('Plot.json') as f:
     file = json.load(f)
 
 npzfile = np.load(file["File name"])
-
-N = 8
-nstates = 2**N
-ham = 'pm'
-model = 'TDT-lrf'
-bias = 'y'
-lrcurve = 3
-dict = {'n':'#1f77b4', 'y':'#d62728'}
-dict2 = {3:'Best', 4:'Worst'}
-
-hist_data = np.absolute(npzfile['arr_1'][-2])
-hist_data = hist_data[~np.isnan(hist_data)]
+config_set = npzfile['arr_0']
+histogram_log = npzfile['arr_2']
 
 tickfont = 13
 labelfont = 14
-
 bins = 10.**np.arange(-16.,2.,1.)
-fig, ax = plt.subplots()
-#fig.suptitle('Model = "'+model+'"; AFH-sign-negative; J = ' + str(J) + '; N = '+str(N)+'; #epochs = $10^{6}$', fontsize=11)
-pyplot.hist(hist_data, bins,color = dict[bias],rwidth=0.9 , log = False)#, label='bias = False')
-pyplot.xscale('log')
-pyplot.legend(loc='upper left', fontsize = tickfont)
-pyplot.yticks(fontsize = tickfont)
-pyplot.xticks(fontsize = tickfont)
-ax.yaxis.set_major_locator(MaxNLocator(integer=True))
-ax.set_ylabel("Counts", fontsize = labelfont)
-ax.set_xlabel("Relative error ($\\Delta E$)", fontsize = labelfont)
-fig.savefig(model+'_AFH-'+ham+'_N'+str(N)+'_bm_Plot.pdf')
+
+with PdfPages(file["File name"][0:-12]+'Histogram.pdf') as pdf:
+    for i in range(len(histogram_log)):
+        fig, ax = plt.subplots()
+        fig.suptitle('Layer-configuration: '+str(config_set[i]), fontsize=labelfont)
+        pyplot.hist(histogram_log[i], bins,color = '#1f77b4',rwidth=0.9 , log = False)#, label='bias = False')
+        pyplot.xscale('log')
+        #pyplot.legend(loc='upper left', fontsize = tickfont)
+        pyplot.yticks(fontsize = tickfont)
+        pyplot.xticks(fontsize = tickfont)
+        ax.yaxis.set_major_locator(MaxNLocator(integer=True))
+        ax.set_ylabel("Counts", fontsize = labelfont)
+        ax.set_xlabel("Relative error ($\\Delta E$)", fontsize = labelfont)
+        pdf.savefig()
+        plt.close()
